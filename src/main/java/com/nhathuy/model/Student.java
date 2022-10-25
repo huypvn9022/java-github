d9569c674a1b717bb008651dee621695baf6cd73
Nhat Huy <phanhuy9022@gmail.com>
2022-10-25 23:48:03 +0700

    Five commit update file Student.java

--- src/main/java/com/nhathuy/model/Student.java
+++ src/main/java/com/nhathuy/model/Student.java
@@ -4,7 +4,7 @@ public class Student {
 	public static void main(String[] args) {
 		int a = 1;
 		int b = 2;
-		if(a == b) {
+		if(a < b && a == 0) {
 			System.out.println("True");
 		} else {
 			System.out.println("False");

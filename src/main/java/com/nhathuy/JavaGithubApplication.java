801f264191cb29caa71a13f7a0e597b71fa9e371
Nhat Huy <phanhuy9022@gmail.com>
2022-10-25 23:27:24 +0700

    First commit file java

---
+++ src/main/java/com/nhathuy/JavaGithubApplication.java
@@ -0,0 +1,13 @@
+package com.nhathuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaGithubApplication.class, args);
+	}
+
+}

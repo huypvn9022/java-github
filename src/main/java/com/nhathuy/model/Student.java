4dd6d6291a6224769a42f35742d5bcdcb9ac1981
Nhat Huy <phanhuy9022@gmail.com>
2022-10-25 23:46:26 +0700

    Four commit update file Student.java

--- src/main/java/com/nhathuy/model/Student.java
+++ src/main/java/com/nhathuy/model/Student.java
@@ -1,5 +1,13 @@
 package com.nhathuy.model;
 
 public class Student {
-	
+	public static void main(String[] args) {
+		int a = 1;
+		int b = 2;
+		if(a == b) {
+			System.out.println("True");
+		} else {
+			System.out.println("False");
+		}
+	}
 }
